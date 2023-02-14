3ef822386f223f4bc159fb3c5cc22120f5ca38b5
Centeno <ntcenteno@quinnipiac.edu>
2023-02-13 20:20:44 -0500

    Step 2

--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,15 @@ public class Calculator{
         System.out.println( "Please Enter Two Numbers");
         int num1 = reader.nextInt();
         int num2 = reader.nextInt();
-        int numTotal = num1 + num2;
+        System.out.println("Do you want to add or subtract?");
+        char sign = reader.next().charAt(0);
+        int numTotal = 0;
+        if(sign == '+'){
+            numTotal = num1 + num2;
+        }
+        else if(sign == '-'){
+            numTotal = num1 - num2;
+        }
         System.out.println(numTotal);
     }
 }
