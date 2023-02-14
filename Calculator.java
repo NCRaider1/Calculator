ec6a86acd15d718df1a5858065b7fd2da7780aa2
Centeno <ntcenteno@quinnipiac.edu>
2023-02-13 20:31:16 -0500

    Step 1: Addition and Subtraction with Basic Whole Numbers

--- Calculator.java
+++ Calculator.java
@@ -4,22 +4,16 @@ public class Calculator{
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
         System.out.println( "Please Enter Two Numbers");
-        float num1 = reader.nextFloat();
-        float num2 = reader.nextFloat();
+        int num1 = reader.nextInt();
+        int num2 = reader.nextInt();
         System.out.println("Do you want to add or subtract?");
         char sign = reader.next().charAt(0);
-        float numTotal = 0;
+        int numTotal = 0;
         if(sign == '+'){
             numTotal = num1 + num2;
         }
         else if(sign == '-'){
             numTotal = num1 - num2;
-        }
-        else if(sign == '*'){
-            numTotal = num1 * num2;
-        }
-        else if(sign == '/'){
-            numTotal = num1 / num2;
         }
         System.out.println(numTotal);
     }
